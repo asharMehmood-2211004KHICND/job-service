6437fbfa55a006d17d96521b470a5dad5137a5fd
asharMehmood-2211004KHICND <ashar.mehmood@xloopdigital.com>
2023-03-09 16:29:21 +0500

    added test commit

--- src/main/java/com/xloop/resourceloop/createJob/CreateJobApplication.java
+++ src/main/java/com/xloop/resourceloop/createJob/CreateJobApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class CreateJobApplication {
-
+	 // gitHub test
 	public static void main(String[] args) {
 		SpringApplication.run(CreateJobApplication.class, args);
 	}
